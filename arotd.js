79c3c7cbcbb62650e6d8d464fcd65f0426df6a48
leider <derleider@web.de>
2013-03-02 14:02:42 +0100

    More questions, first last

--- arotd.js
+++ arotd.js
@@ -9,7 +9,7 @@
     $.convert_list = function() {
         for (i in $._questions) {
             var q = $._questions[i]
-            $.questions.push(new Question(q[0], q[1], i))
+            $.questions.push(new Question(q[0], q[1], q[2]))
         }
     }
     $.convert_list()
@@ -21,7 +21,7 @@
         $.number.html(current.number)
     }
 
-    $.next_question_id = function() {
+    $.previous_question_id = function() {
         $.i++
         if ($.i > $.questions.length - 1) {
             $.i = 0
@@ -30,7 +30,7 @@
         $.set_id($.i)
     }
 
-     $.previous_question_id = function() {
+     $.next_question_id = function() {
         $.i--
         if ($.i < 0) {
             $.i = $.questions.length - 1
@@ -45,12 +45,26 @@
         $.set_id($.i)
      }
 
-    $.fn.arotd = function(question, author, previous, next, random, number) {
+     $.first_question_id = function() {
+		$.i = $.questions.length - 1
+        $.display_current()
+        $.set_id($.i)
+     }
+
+     $.last_question_id = function() {
+     	$.i = 0
+        $.display_current()
+        $.set_id($.i)
+     }
+
+    $.fn.arotd = function(question, author, number, first, previous, random, next, last) {
         $.question = $(question)
         $.author = $(author)
-        $.previous = $(previous)
-        $.random = $(random)
         $.number = $(number)
+		$.first = $(first)
+		$.previous = $(previous)
+		$.random = $(random)
+		$.last = $(last)
 
         $.get_id($.display_current)
 
@@ -67,6 +81,16 @@
         $.random.unbind()
         $.random.click(function() {
             $.random_question_id()
+        })
+
+        $.first.unbind()
+        $.first.click(function() {
+            $.first_question_id()
+        })
+
+        $.last.unbind()
+        $.last.click(function() {
+            $.last_question_id()
         })
     }
 
@@ -86,7 +110,9 @@
             sortable.push([author, counted[author]])
         }
         function compare(sortable_1, sortable_2) {
-            return sortable_2[1] - sortable_1[1]
+            if ($.realname(sortable_1[0]).toUpperCase() < $.realname(sortable_2[0]).toUpperCase()) return -1;
+            if ($.realname(sortable_1[0]).toUpperCase() > $.realname(sortable_2[0]).toUpperCase()) return 1;
+			return 0;
         }
 
         var sorted = sortable.sort(compare)
@@ -98,7 +124,15 @@
  
 	$.link = function(name) {
 		if (name.charAt(0) == "@") {
-			return "<a href='http://twitter.com/" + name.substr(1) + "'>" + name + "</a>";
+			return "<a href='http://twitter.com/" + name.substr(1) + "'>" + name.substr(1)  + "</a>";
+		} else {
+			return name;
+		}
+	}
+
+	$.realname = function(name) {
+		if (name.charAt(0) == "@") {
+			return name.substr(1);
 		} else {
 			return name;
 		}
